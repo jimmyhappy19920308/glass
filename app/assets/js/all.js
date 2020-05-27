e419435966a7246026c8cab65ba2baea6ca2e208
yingming25 <karcnonf@gmail.com>
2020-05-27 16:10:38 +0800

    新增 FAQ 選單摺疊效果

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -3,4 +3,12 @@ $(document).ready(function() {
 		e.preventDefault();
 		$('body').toggleClass('menu-show');
 	});
+
+	$('.faq-item').on('click', function(e) {
+		e.preventDefault();
+		$(this).children().children('.faq-question-icon').toggleClass('active');
+		$(this).toggleClass('active-answer');
+		$(this).siblings().children().children('.faq-question-icon').removeClass('active');
+		$(this).siblings().removeClass('active-answer');
+	})
 });
